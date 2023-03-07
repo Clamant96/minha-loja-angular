3e9c1ed8a2d07f497c57c872633f0ac7ee00863c
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2023-03-07 15:49:22 -0300

    Gerenciamente de produtos, com a implementacao de endpoints de criacao e edcao de produtos dentro de seus respectivos blocos.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { CategoriaService } from './../service/categoria.service';
+import { Categoria } from './../model/Categoria';
 import { UsuarioService } from './../service/usuario.service';
 import { Usuario } from './../model/Usuario';
 import { Router } from '@angular/router';
@@ -14,13 +16,21 @@ import { environment } from 'src/environments/environment.prod';
 export class HomeComponent implements OnInit {
 
   public listaDeProdutos: Produto[] = [];
+  public listaDeCategorias: Categoria[] = [];
   public id: number = environment.id;
 
   public carrinho: Usuario = new Usuario();
 
+  public gerenciaProduto: Produto = new Produto();
+  public gerenciaProdutoEdcao: Produto = new Produto();
+  public gerenciaCategoria: Categoria = new Categoria();
+
+  public isEdicaoProduto: boolean = false;
+
   constructor(
     private produtoService: ProdutoService,
     private usuarioService: UsuarioService,
+    private categoriaService: CategoriaService,
     private router: Router
 
   ) { }
@@ -33,7 +43,10 @@ export class HomeComponent implements OnInit {
 
     }
 
+    this.gerenciaProduto.img = "https://cdn-icons-png.flaticon.com/512/1695/1695213.png";
+
     this.getAllProdutos();
+    this.getAllCategorias();
 
   }
 
@@ -41,6 +54,15 @@ export class HomeComponent implements OnInit {
 
     this.produtoService.getAllProdutos().subscribe((resp: Produto[]) => {
       this.listaDeProdutos = resp;
+
+    });
+
+  }
+
+  getAllCategorias() {
+
+    this.categoriaService.getAllCategorias().subscribe((resp: Categoria[]) => {
+      this.listaDeCategorias = resp;
 
     });
 
@@ -78,6 +100,70 @@ export class HomeComponent implements OnInit {
 
     }, erro => {
       alert('Ocorreu um erro ao tentar comprar o produto.');
+
+    });
+
+  }
+
+  changeCategoria(event: any) {
+    this.gerenciaCategoria.id = event.target.value;
+
+    console.log(this.gerenciaCategoria);
+
+  }
+
+  cadastrarProduto(produto: Produto) {
+
+    produto.categoria = this.gerenciaCategoria;
+
+    this.produtoService.postProduto(produto).subscribe((resp: Produto) => {
+      this.gerenciaProduto = new Produto();
+      this.gerenciaCategoria = new Categoria();
+
+      this.gerenciaProduto.img = "https://cdn-icons-png.flaticon.com/512/1695/1695213.png";
+
+      this.getAllProdutos();
+
+    }, erro => {
+      alert('Ocorreu um erro ao tentar cadastrar o produto.');
+
+    });
+
+  }
+
+  gerenciaBotaoEdicaoProduto(estado: boolean, produto: Produto) {
+
+    if(estado) {
+      this.isEdicaoProduto = false;
+
+    }else {
+      this.isEdicaoProduto = true;
+
+      this.gerenciaProdutoEdcao = new Produto();
+      this.gerenciaProdutoEdcao = produto;
+
+    }
+
+    return estado;
+  }
+
+  atualizarProduto(produto: Produto) {
+
+    this.produtoService.putProduto(produto).subscribe((resp: Produto) => {
+      this.gerenciaProduto = new Produto();
+      this.gerenciaProdutoEdcao = new Produto();
+      this.gerenciaCategoria = new Categoria();
+
+      this.isEdicaoProduto = false;
+
+      this.gerenciaProduto.img = "https://cdn-icons-png.flaticon.com/512/1695/1695213.png";
+
+      this.getAllProdutos();
+
+    }, erro => {
+      alert('Ocorreu um erro ao tentar atualizar o produto.');
+
+      this.isEdicaoProduto = false;
 
     });
 
